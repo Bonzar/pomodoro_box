44c5295b3b4e2184e1df72929d47b24ce1489249
bonzar <nva112002@gmail.com>
2023-06-11 15:17:08 +0300

    fix sunday show as new week

--- src/helpers/js/getCurrentWeekCornerDays.ts
+++ src/helpers/js/getCurrentWeekCornerDays.ts
@@ -1,7 +1,10 @@
+import { getTodayWeekDayIndex } from "./getTodayWeekDayIndex.ts";
+
 export const getCurrentWeekCornerDays = () => {
   const today = new Date();
 
-  const firstDayOfCurrentWeekInMonth = today.getDate() - today.getDay() + 1;
+  const firstDayOfCurrentWeekInMonth =
+    today.getDate() - getTodayWeekDayIndex() + 1;
   const lastDayOfCurrentWeekInMonth = firstDayOfCurrentWeekInMonth + 7;
 
   const firstWeekDayDate = new Date(
