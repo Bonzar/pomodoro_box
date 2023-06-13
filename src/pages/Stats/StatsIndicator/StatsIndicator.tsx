f1a606c466014b680aa12c82d4e9baa7bf9ce215
bonzar <nva112002@gmail.com>
2023-06-13 23:28:12 +0300

    removed styling for generic modal component add transition on statsIndicator color changing

--- src/pages/Stats/StatsIndicator/StatsIndicator.tsx
+++ src/pages/Stats/StatsIndicator/StatsIndicator.tsx
@@ -4,6 +4,8 @@ import { Heading } from "../../../components/ui/Heading";
 import { TextEl } from "../../../components/ui/TextEl";
 import { Icon } from "../../../components/ui/Icon/Icon.tsx";
 import type { TColorActive } from "../../../assets/types/TColor.ts";
+import { getClassName } from "../../../helpers/react/getClassName.ts";
+import type { CSSProperties } from "react";
 
 interface IStatsIndicatorProps {
   name: string;
@@ -20,20 +22,21 @@ export const StatsIndicator = ({
   color,
   isNoData = false,
 }: IStatsIndicatorProps) => {
+  let indicatorStyles: CSSProperties | undefined;
+  let iconStyles: CSSProperties | undefined;
+  if (!isNoData) {
+    indicatorStyles = { backgroundColor: `var(--${color}-light)` };
+    iconStyles = { color: `var(--${color})` };
+  }
+
   return (
     <div
-      className={styles.indicator}
-      style={{
-        backgroundColor: isNoData ? `var(--gray-F4)` : `var(--${color}-light)`,
-      }}
+      className={getClassName([styles.indicator, isNoData && styles.noData])}
+      style={indicatorStyles}
     >
       <Heading as="h2">{name}</Heading>
       <TextEl className={styles.value}>{value}</TextEl>
-      <Icon
-        className={styles.icon}
-        iconName={icon}
-        iconColor={isNoData ? "gray-C4" : color}
-      />
+      <Icon className={styles.icon} iconName={icon} style={iconStyles} />
     </div>
   );
 };
