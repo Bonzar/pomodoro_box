0571eba3ebaaf8f3a18ec61ebdb8fe412423fd0f
Vladislav Navoyan <nva112002@gmail.com>
2023-06-15 01:37:29 +0300

    set fixed row size for stats cells (#19)
    fix statsIndicator icon overflow container in some window sizes

--- src/pages/Stats/StatsIndicator/StatsIndicator.tsx
+++ src/pages/Stats/StatsIndicator/StatsIndicator.tsx
@@ -22,18 +22,20 @@ export const StatsIndicator = ({
 }: IStatsIndicatorProps) => {
   return (
     <div
-      className={styles.indicator}
+      className={styles.indicatorWrapper}
       style={{
         backgroundColor: isNoData ? `var(--gray-F4)` : `var(--${color}-light)`,
       }}
     >
-      <Heading as="h2">{name}</Heading>
-      <TextEl className={styles.value}>{value}</TextEl>
-      <Icon
-        className={styles.icon}
-        iconName={icon}
-        iconColor={isNoData ? "gray-C4" : color}
-      />
+      <div className={styles.indicator}>
+        <Icon
+          className={styles.icon}
+          iconName={icon}
+          iconColor={isNoData ? "gray-C4" : color}
+        />
+        <Heading as="h2">{name}</Heading>
+        <TextEl className={styles.value}>{value}</TextEl>
+      </div>
     </div>
   );
 };
