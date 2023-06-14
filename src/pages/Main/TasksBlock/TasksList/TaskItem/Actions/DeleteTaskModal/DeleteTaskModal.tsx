1c2ddfd5fbc506e8d4ef5b50da8d984931dcd730
Vladislav Navoyan <nva112002@gmail.com>
2023-06-15 01:56:11 +0300

    Features/remove z index (#18)
    * changes in use of z-index
    fix header appear along delete modal
    
    * add isOpen prop

--- src/pages/Main/TasksBlock/TasksList/TaskItem/Actions/DeleteTaskModal/DeleteTaskModal.tsx
+++ src/pages/Main/TasksBlock/TasksList/TaskItem/Actions/DeleteTaskModal/DeleteTaskModal.tsx
@@ -6,50 +6,73 @@ import { Indent } from "../../../../../../../components/ui/Indent";
 import { Button } from "../../../../../../../components/ui/Button";
 import { useAppDispatch } from "../../../../../../../store/hooks.ts";
 import { deleteTaskThank } from "../../../../../../../store/deleteTaskThank.ts";
+import { CSSTransition } from "react-transition-group";
+import { useRef } from "react";
 
 interface IDeleteTaskModalProps {
   id: string | number;
   onClose: VoidFunction;
+  isOpen: boolean;
 }
 
-export const DeleteTaskModal = ({ id, onClose }: IDeleteTaskModalProps) => {
+export const DeleteTaskModal = ({
+  id,
+  onClose,
+  isOpen,
+}: IDeleteTaskModalProps) => {
   const dispatch = useAppDispatch();
 
   const handleConfirmDeleting = () => dispatch(deleteTaskThank({ id }));
+  const ref = useRef<HTMLDivElement>(null);
 
   return (
     <Modal onOutsideClick={onClose}>
-      <div className={styles.modalWrapper} />
-      <div className={styles.modal}>
-        <button className={styles.closeBtn} onClick={onClose}>
-          <Icon iconName="cross" iconColor="gray-C4" />
-        </button>
-        <TextEl textSize={24} textLineHeight={33}>
-          Удалить задачу?
-        </TextEl>
-        <Indent size={25} />
-        <Button
-          btnColor="red"
-          btnFilled
-          onClick={() => {
-            handleConfirmDeleting();
-            onClose();
-          }}
-        >
-          Удалить
-        </Button>
-        <Indent size={10} />
-        <button>
-          <TextEl
-            textWeight={300}
-            textLineHeight={24}
-            className={styles.cancelBtn}
-            onClick={onClose}
-          >
-            Отмена
-          </TextEl>
-        </button>
-      </div>
+      <CSSTransition
+        in={isOpen}
+        nodeRef={ref}
+        timeout={175}
+        classNames={{
+          enter: styles.deleteModalEnter,
+          enterActive: styles.deleteModalEnterActive,
+          exit: styles.deleteModalExit,
+          exitActive: styles.deleteModalExitActive,
+        }}
+        unmountOnExit
+      >
+        <div ref={ref} className={styles.deleteModal}>
+          <div className={styles.deleteModalWrapper} />
+          <div className={styles.deleteModalInner}>
+            <button className={styles.closeBtn} onClick={onClose}>
+              <Icon iconName="cross" iconColor="gray-C4" />
+            </button>
+            <TextEl textSize={24} textLineHeight={33}>
+              Удалить задачу?
+            </TextEl>
+            <Indent size={25} />
+            <Button
+              btnColor="red"
+              btnFilled
+              onClick={() => {
+                handleConfirmDeleting();
+                onClose();
+              }}
+            >
+              Удалить
+            </Button>
+            <Indent size={10} />
+            <button>
+              <TextEl
+                textWeight={300}
+                textLineHeight={24}
+                className={styles.cancelBtn}
+                onClick={onClose}
+              >
+                Отмена
+              </TextEl>
+            </button>
+          </div>
+        </div>
+      </CSSTransition>
     </Modal>
   );
 };
