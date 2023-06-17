16ee8b39b96f24ea30e07ea37246c264d9dc5021
Vladislav Navoyan <nva112002@gmail.com>
2023-06-17 22:02:51 +0300

    add slash to base path (#44)

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/pomodoro_box",
+  base: "/pomodoro_box/",
   plugins: [
     react(),
     svgr(),
